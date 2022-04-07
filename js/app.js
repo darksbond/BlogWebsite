86adb9468b9973164181bab135c225468ec01c91
Mushfik F.Rahman <mushfik.agamilabs@gmail.com>
2022-04-07 15:24:02 +0600

    revmap some code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(function () {
-
-  let ss = location.href;
+ //!------ getting location href and linking to the pages
+  //? ---- 3rd step 
+ /*  let ss = location.href;
  
   let index = ss.indexOf("?");
   let subIndex = -1;
@@ -17,11 +18,15 @@ $(function () {
     else {
       page = substring;
     }
-  }
+  } */
   
-
+  
+  
+  //! -------   Getting json data and load it to main page
+  //? ---------- 1st step
+  
+  
   $.get("lib/data.json", (data) => {
-   
     data.forEach((item,index) => {
       let template = $(`<h2 class="accordion-header" id="heading${item.id}">
           <button
@@ -51,59 +56,60 @@ $(function () {
             </ul>
           </div>
         </div>`)
-        .appendTo('.accordion');
+        .appendTo('.left-menu-bar');
       
-      //item is stored in .accordion-button element
+      //? ------  item is stored in .accordion-button element
       template.find(`.accordion-button`).data("itemdata", item);
 
-     
-      if (page.length) {
+     /*  if (page.length) {
         var title = item.title.toLowerCase().replace(/ /g, "-");
-
         if (page == title) {
           template.find(`.accordion-button`).trigger("click");
         }
-        //console.log(page);
-
-      } else if (index == 0) {
-       // console.log(index);
-        template.find(`.accordion-button`).trigger("click");
       }
- 
+
+      else if (index == 0) {
+        template.find(`.accordion-button`).trigger("click");
+      } */
+
     });
   });
 
-   
+
+  // !----------- Adding Event on Accordion button
+  //? ---------  2nd step 
   $(document).on("click", '.accordion-button', (e) => {
-    let data = $(e.target).data("itemdata");
-    
+    let data = $(e.target).data("itemdata"); 
     let innerContent = $(e.target).data("inner_content");
-    getHref(innerContent);
+
 //Using Conditions for not loading same location data
+    //this condition is used for not fetching the same location
+
     if (innerContent && innerContent.length) {
+     
       $(`#main-content`).html(innerContent);
       getHref();
     }
     else {
       $.get(data.location, (htmlContent) => {
-        
         $(e.target).data("inner_content", htmlContent);
         $(`#main-content`).html(htmlContent);
         getHref();
-      }, "html");
+
+      },"html");
     }
 
   });
 
+//! -------------   getHref Function
+  //? ----------   4th step 
   function getHref() {
-    let hrefsTag = $("#main-content a[href^='#']").filter((b, a) => $(a).attr("href").length > 1);
+    let hrefsTag = $("#main-content a[href^='#']").filter((index, element) => $(element).attr("href").length > 1);
     
-    hrefsTag.each((index,element) => {
+    hrefsTag.clone().each((index,element) => {
       $('.right-bar-content').append(element);
-    })
-    
-
+    })  
 }
 
-
+  
 })
